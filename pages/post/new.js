561b4ccd35d0f77f350c13ae4f84995506c4e3d8
Roland Pákai <roland.pakai@liferay.com>
2023-03-24 18:24:42 +0100

    Styling updates & loading icon

--- pages/post/new.js
+++ pages/post/new.js
@@ -1,60 +1,86 @@
 import { useState } from "react";
-import { userRouter } from "next/router";
+import { useRouter } from "next/router";
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 import { AppLayout } from "../../components/AppLAyout";
 import { getAppProps } from "../../utils/getAppProps";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBrain } from "@fortawesome/free-solid-svg-icons";
 export default function NewPost() {
-  const router = userRouter();
+  const router = useRouter();
   const [topic, setTopic] = useState("");
   const [keywords, setKeywords] = useState("");
+  const [generating, setGenerating] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setGenerating(true);
 
-    const response = await fetch('/api/generatePost', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ topic, keywords })
-    });
-
-    const json = await response.json();
-    if (json?.postId) {
-      router.push(`/post/${json.postId}`);
+    try {
+      const response = await fetch('/api/generatePost', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ topic, keywords })
+      });
+  
+      const json = await response.json();
+  
+      if (json?.postId) {
+        router.push(`/post/${json.postId}`);
+      }
+    } catch (error) {
+      setGenerating(false);
     }
   };
 
   return (
-  <div>
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>
-          <string>
-            Generate a blog post on the topic of
-          </string>
-        </label>
-        <textarea 
-          className="txtarea"
-          value={topic} 
-          onChange={e => setTopic(e.target.value)}/>
+  <div className="h-full overflow-hidden">
+    {!!generating && (
+      <div className="text-green-500 flex h-full animate-pulse w-full flex-col justify-center items-center">
+        <FontAwesomeIcon icon={faBrain} className="text-8xl"/>
+        <h6>Generating...</h6>
       </div>
-      <div>
-        <label>
-          <string>
-            Targeting the following keywords
-          </string>
-        </label>
-        <textarea 
-          className="txtarea"
-          value={keywords} 
-          onChange={e => setKeywords(e.target.value)}/>
-      </div>
-      <button type="submit" className="btn" >
-        Generate
-      </button>
-
-    </form>
+    )}
+    {!generating && (
+    <div className="w-full h-full flex flex-col overflow-auto">
+      <form 
+        onSubmit={handleSubmit}
+        className="m-auto w-full max-w-screen-sm bg-slate-100 p-4 rounded-md shadow-xl border border-slate-200 shadow-slate-200"
+      >
+        <div>
+          <label>
+            <strong>
+              Generate a blog post on the topic of
+            </strong>
+          </label>
+          <textarea 
+            className="txtarea"
+            value={topic} 
+            onChange={e => setTopic(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>
+            <strong>
+              Targeting the following keywords
+            </strong>
+          </label>
+          <textarea 
+            className="txtarea"
+            value={keywords} 
+            onChange={e => setKeywords(e.target.value)}
+          />
+          <small className="block mb-2">
+            Separate the keywords with a comma
+          </small>
+        </div>
+        <button type="submit" className="btn" >
+          Generate
+        </button>
+      </form>
+    </div> 
+    )}
   </div>
   );
 }
